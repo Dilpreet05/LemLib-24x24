16504fb99ec134e93a59e9b29989fb176eaa4d3b
Dilpreet <67494637+Dilpreet05@users.noreply.github.com>
2024-08-24 20:26:59 -0400

    Minor addition to Odom Comments & Improved comments
    Added comments explaining odom, what we use for odom, and creating an IMU. Using the better comments extension, I improved comments by adding specifiers for major and minor points in comments, improves clarity for readers & Engineering Notebook code pictures

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include "main.h"
+#include "pros/imu.hpp"
 
 
 /* Motor Groups declaration and initialization */
 
-// left_motors represents the grouping of motors that are on the LEFT side of the drive-train
+/**
+ * 
+ ** left_motors represents the grouping of motors that are on the LEFT side of the DRIVETRAIN
+ 
+ * ARGUMENTS:
+ * 
+ ** The first argument in left_motors represents the ports that each motor is connected to.
+ * ! in this case our motor ports will be negative, negativity in motor ports indicates that the motor should be spinning in a reversed direction.
+ * ! with a positive port, motors spin clockwise at a positive input. With a negative port, motors spoin counterclockwise at a positive input.
+ * 
+ ** The second argument represents the gearset of the motor
+ * ! There are different types of gearsets: Red, Green, and Blue.
+ * ! Blue being the fastest at 600PM, Red being the slowest at 100RPM, and Green being the middle range at 200RPM.
+ * 
+ */
 /* Arguments:
 
  * The first argument in left_motors represents the ports that each motor is connected to
@@ -17,40 +32,52 @@
  */
 pros::MotorGroup left_motors({-1, -2, -3}, pros::MotorGearset::blue); // Motor ports {1,2,3} at 600RPM (Blue gearset)
 
-// same logic from left_motors applies to right_motors, including arguments and motor gearsets.
-// right_motors represents the grouping of motors that are on the RIGHT side of the drive-train
-// positive motor ports because the motors should be spinning clockwise for the robot to move forward.
+
+
+/**
+ ** Same logic from left_motors applies to right_motors, including arguments and motor gearsets.
+ ** Right_motors represents the grouping of motors that are on the RIGHT side of the DRIVETRAIN
+ ** Positive motor ports because the motors should be spinning clockwise for the robot to move forward.
+ */
 pros::MotorGroup right_motors({4, 5, 6}, pros::MotorGearset::blue); // Motor ports {4,5,6} at 600RPM
 
 /* End of Motor Group declaration and initialization */
 
 
-/* Start of Drive Train code */
+/* Start of DRIVETRAIN code */
 
 /**
- * This block of code creates a drivetrain object,
+ * ! This block of code creates a DRIVETRAIN object,
  * 
- * This stores our left side motors, right side motors, our track width, wheel diameter, Drive RPM, and our horizontal drift value
+ *  
+ ** This stores our left side motors, right side motors, our TRACKWIDTH, WHEELDIAMETER, 
+ ** DRIVERPM, and our HORIZONTALDRIFT value 
  *
- * Track Width - the distance between the left side and right sides of the drivetrain. 
- *				 Specifically, from the middle of the wheels.
- *
- * Wheel diameter - The diameter of our drive train wheels. We measured using calipers.
- *
- * Drive RPM - The speed our wheels will spin at, this value is different from the Motor RPM because we're using a gear ratio,
- *			   Our drive train gearings consists of a 36t Gear input and a 48t output, this comes out to be a 3/4 ratio or 0.75,
- *			   We take this 0.75 value and multiply it by our motor RPM to find our drivetrain/wheel RPM,
- *			   Our robot uses a 600RPM motor with a 0.75 gear ratio, making our drivetrain RPM become 450RPM.
- *
- * Horizontal Drift -  A feature that ensures compatibility with mutliple types of drivetrains , 
- *					   with both all omni wheels (drift drive), or drivetrains with center traction wheels.
- *					   It controls how fast the chassis can move while turning, a higher value means 
- *					   The robot has less physical drift when driving and turning in auton and lower value means the opposite.
- *					   We use our default value of 8 because we have center traction wheels which allows us to grip the field better.
- *
+ * TRACKWIDTH:
+ * ! The distance between the left side and right side of the DRIVETRAIN.
+ * ! Specifically from the middle of the wheels.
+ * 
+ * WHEELDIAMETER:
+ * ! The diameter of our DRIVETRAIN wheels. We measured using calipers.
+ * 
+ * DRIVERPM:
+ * ! The speed our wheels will spin at, this value is different from the motor RPM because we are using a gear ratio,
+ * ! Our DRIVETRAIN gearing consists of a 36T gear input and a 48T gear output,  this comes out to be a 3/4 ratio or 0.75,
+ * ! We take this 0.75 value and multiply it by our motor RPM to find our DRIVETRAIN wheel RPM,
+ * ! Our robot uses a 600RPM motor with a 0.75 gear ratio, making our DRIVETRAIN RPM become 450RPM.
+ * 
+ * HORIZONTALDRIFT:
+ * ! A feature that ensures compatibility with multiple types of DRIVETRAINS,
+ * ! with both all omni wheels (drift drive), or DRIVETRAINS with center traction wheels.
+ * ! It controls how fast the chassis can move while turning, a higher value means,
+ * ! The robot has less physical ddrift when driving and turning in auton and lower value means the opposite.
+ * ! We use our default value of 8 because we have center traction wheels which allow us to grip the field more proper.
+ * b
+ * 
  */
 
-lemlib::Drivetrain drivetrain(&left_motors, // left motor group
+
+lemlib::Drivetrain DRIVETRAIN(&left_motors, // left motor group
                               &right_motors, // right motor group
                               11.5, // Track width: 11.5 INCHES
                               2.75, // Wheel diameter of our 3D Printed custom Omni Wheels & VEX Traction wheels
@@ -59,10 +86,43 @@ lemlib::Drivetrain drivetrain(&left_motors, // left motor group
 );
 
 
-/*  End of Drive Train Code*/
+/*  End of Drive Train Code */
+
+
+/* Start of Odomoetry Code */
+
+/**
+ * 
+ *?s What is odometry?
+ ** Odometry is the use of data from motion sensors to estimate change in position over time,
+ ** It is used in robotics by some legged or wheeled robots to estimate their position relative to a starting location.
+ ** In our case, we will be using traacking wheels in order to track our robots position accurately and percisely.
+ * 
+ *? Why use odometry?
+ ** We use odometry for consistincy of autonomous rutines including both skills autonomous and match autonomous.
+ * 
+ */
+
+
+/**
+ * 
+ *? For our odometry, we use readings from 2 unique sensors and 3 total sensors:
+ **  1. An IMU (Inertial Measurement Unit) using a Vex V5 Inertial Sensor
+ **  2. Vertical Tracking wheel using a Vex V5 Rotation Sensor
+ **  3. Horizonal Tracking wheel using a Vex V5 Rotation Sensor.
+ * 
+ * IMU:
+ * 	! this is used in order to find our current heading value which is the current direction the robot is facing.
+ * 	! we can use this value in our odometry to make sure that our robot faces the proper direction when commanded to.
+ * 
+ * 
+ */
+pros::Imu IMU(10);
 
 
 
+
+/* End of Odometry Code*/
 
 
 
