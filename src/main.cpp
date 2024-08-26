7d9386eec3d1aba3c883fd8fd70b0fe6e540f634
Dilpreet <67494637+Dilpreet05@users.noreply.github.com>
2024-08-26 14:43:07 -0400

    Major code updates, no comments
    Created a controller (MASTER), began tracking wheel code and comments, improved initialize function, created sensors, and PID things in order to create a chassis and drive the robot for intake testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "main.h"
+#include "lemlib/chassis/trackingWheel.hpp"
+#include "pros/abstract_motor.hpp"
 #include "pros/imu.hpp"
+#include "pros/misc.h"
+#include "pros/misc.hpp"
+#include "pros/rotation.hpp"
 
+pros::Controller master(pros::E_CONTROLLER_MASTER);
 
 /* Motor Groups declaration and initialization */
 
@@ -121,8 +127,119 @@ pros::Imu IMU(10);
 
 
 
+/**
+ * 
+ * HORIZONTALTRACKINGSENSOR:
+ * ! tracks any movement in the horizontal direction of our robot, this is use a lot in the case where our auton paths stray
+ * ! due to horizontal contact from our or opposing alliance robots.
+ * 
+ * VERICALTRACKINGSENSOR:
+ * ! tracks vertical movement/straight movements of the robot. 
+ * ! This allows us to accurately move our robot a specific amount of units forward, as well as account for any contact.
+ * 
+ * 
+ */
+pros::Rotation horizontalTrackingWheelSensor(12);
+pros::Rotation verticalTrackingWheelSensor(13);
+
+
+
+lemlib::TrackingWheel horizontalTrackingWheel(&horizontalTrackingWheelSensor,2.76,0);
+lemlib::TrackingWheel verticalTrackingWheel(&verticalTrackingWheelSensor,2.76,0);
+
+
+
+lemlib::OdomSensors sensors(&verticalTrackingWheel, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            &horizontalTrackingWheel, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &IMU // inertial sensor
+);
 
 /* End of Odometry Code*/
+
+/* Start of PID Code */
+
+// lateral PID controller
+lemlib::ControllerSettings lateralController (10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angularController (2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+
+/* End of PID Code */
+
+/* Start of chassis */
+
+lemlib::Chassis chassis(DRIVETRAIN, // drivetrain settings
+						lateralController, // lateral PID settings
+						angularController, // angular PID settings
+						sensors // odometry sensors
+);
+
+
+/* End of Chassis */
+
+
+
+/* Start of Intake Code */
+
+// placeholder motor ports.
+pros::Motor flexWheelIntakeMotor(10,pros::MotorGearset::green,pros::v5::MotorUnits::degrees);
+pros::Motor hookIntakeMotor(11,pros::MotorGearset::green,pros::v5::MotorUnits::degrees);
+
+
+
+void spinIntake(){
+
+	if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){
+		flexWheelIntakeMotor.move(127);
+	}else{
+		flexWheelIntakeMotor.move(0);
+		flexWheelIntakeMotor.brake();
+	}
+
+	if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
+		hookIntakeMotor.move(127);
+	}else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
+		hookIntakeMotor.move(-127);
+	}else{
+		hookIntakeMotor.move(0);
+		hookIntakeMotor.brake();
+	}
+	
+
+
+}
+
+void setIntakeBrakes(){
+
+	flexWheelIntakeMotor.set_brake_mode(pros::MotorBrake::coast);
+	hookIntakeMotor.set_brake_mode(pros::MotorBrake::coast);
+
+}
+
+
+
+/* End of Intake Code */
 
 
 
@@ -135,9 +252,26 @@ pros::Imu IMU(10);
  */
 
 
+// void initialize() {
+// 	pros::lcd::initialize();
+// 	pros::lcd::set_text(1, "Hello PROS User!");
+// 	setIntakeBrakes();
+// }
+
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+    // print position to brain screen
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
 }
 
 /**
@@ -191,15 +325,8 @@ void opcontrol() {
 
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+		spinIntake();
+
 	}
 }
