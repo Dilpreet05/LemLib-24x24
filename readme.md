10a59f01b0ba41361e84a375f53ddb3bdb28bab5
K <Redknclarke@gmail.com>
2024-09-01 19:22:34 -0400

    readme and intake comments

---
+++ readme.md
@@ -0,0 +1,25 @@
+# AYO VEXU
+
+### Description
+
+This program is the code for the AYO 15x15 robot in the 2024-2025 season of high stakes. The main branch is the branch that functions the best, any other branch is likely testing new features without ruining what we already have.
+
+### Software
+
+Currently we use Pros and LemLib to program our robot. We considered using VOSS but decided against it because LemLib fits our use cases more (especially the odometry tool).
+
+### Autons
+
+(WIP)
+
+### User Control
+
+(WIP)
+
+### Ports
+
+(WIP)
+
+### Controls
+
+(WIP)
--- src/main.cpp
+++ src/main.cpp
@@ -244,18 +244,18 @@ pros::Motor hookIntakeMotor(-11,pros::MotorGearset::green,pros::v5::MotorUnits::
 
 void spinIntake(){
 
-	if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){
+	if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){ // Move wheel intake while holding L1
 		flexWheelIntakeMotor.move(127);
-	}else{
+	}else{ // Stop wheel intake otherwise
 		flexWheelIntakeMotor.move(0);
 		flexWheelIntakeMotor.brake();
 	}
 
-	if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
+	if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){ // Move hook intake while holding R1
 		hookIntakeMotor.move(127);
-	}else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
+	}else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){ // Move hook intake the other way while holding L2
 		hookIntakeMotor.move(-127);
-	}else{
+	}else{ // Stop otherwise
 		hookIntakeMotor.move(0);
 		hookIntakeMotor.brake();
 	}
@@ -264,7 +264,7 @@ void spinIntake(){
 
 }
 
-void setIntakeBrakes(){
+void setIntakeBrakes(){ // Set the intake motors to break with coast
 
 	flexWheelIntakeMotor.set_brake_mode(pros::MotorBrake::coast);
 	hookIntakeMotor.set_brake_mode(pros::MotorBrake::coast);
