d03555c66bc277c2e770cf5dc849751cdef217dd
Dilpreet <67494637+Dilpreet05@users.noreply.github.com>
2024-09-16 11:58:01 -0400

    Comments+gif-pros

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include "main.h"
 #include "lemlib/chassis/trackingWheel.hpp"
+#include "liblvgl/llemu.hpp"
 #include "pros/abstract_motor.hpp"
 #include "pros/imu.hpp"
 #include "pros/misc.h"
 #include "pros/misc.hpp"
 #include "pros/rotation.hpp"
+#include "asset.h"
+#include "gif-pros/gifclass.hpp"
+ASSET(spongebob_gif)
 
 
 /* Motor Groups declaration and initialization */
@@ -244,16 +248,18 @@ lemlib::Chassis chassis(DRIVETRAIN,        // drivetrain settings
 
 
 /**
- * Runs initialization code. This occurs as soon as the program is started.
+ *! Runs initialization code. This occurs as soon as the program is started.
  *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
+ ** All other competition modes are blocked by initialize; it is recommended
+ ** to keep execution time for this mode under a few seconds.
  */
 
 void initialize()
 {
     pros::lcd::initialize(); // initialize brain screen
     chassis.calibrate();     // calibrate sensors
+
+
     // print position to brain screen
     pros::Task screen_task([&]()
                            {
@@ -265,59 +271,72 @@ void initialize()
             // delay to save resources
             pros::delay(20);
         } });
+
+
+}
+
+
+/**
+ ** Runs while the robot is in the disabled state of Field Management System or
+ ** the VEX Competition Switch, following either autonomous or opcontrol. When
+ ** the robot is enabled, this task will exit.
+ */
+void disabled() {
+
+    // This displays a funny gif on our brain while our robot is in the disabled state.
+    // psychlogical warefare against our opposing teams.
+    Gif* gif = new Gif(spongebob_gif, lv_scr_act()); 
+
 }
 
 /**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
-
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
+ ** Runs after initialize(), and before autonomous when connected to the Field
+ ** Management System or the VEX Competition Switch. This is intended for
+ ** competition-specific initialization routines, such as an autonomous selector
+ ** on the LCD.
  *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
+ ** This task will exit when the robot is enabled and autonomous or opcontrol
+ ** starts.
  */
 void competition_initialize() {}
 
 /**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
+ ** Runs the user autonomous code. This function will be started in its own task
+ ** with the default priority and stack size whenever the robot is enabled via
+ ** the Field Management System or the VEX Competition Switch in the autonomous
+ ** mode. Alternatively, this function may be called in initialize or opcontrol
+ ** for non-competition testing purposes.
  *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
+ ** If the robot is disabled or communications is lost, the autonomous task
+ ** will be stopped. Re-enabling the robot will restart the task, not re-start it
+ ** from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+
+    pros::lcd::clear();
+
+}
 
 /**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
+ ** Runs the operator control code. This function will be started in its own task
+ ** with the default priority and stack size whenever the robot is enabled via
+ ** the Field Management System or the VEX Competition Switch in the operator
+ ** control mode.
  *
- * If no competition control is connected, this function will run immediately
- * following initialize().
+ ** If no competition control is connected, this function will run immediately
+ ** following initialize().
  *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
+ ** If the robot is disabled or communications is lost, the
+ ** operator control task will be stopped. Re-enabling the robot will restart the
+ ** task, not resume it from where it left off.
  */
 void opcontrol()
 {
-    pros::MotorGroup left_mg({1, -2, 3});   // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-    pros::MotorGroup right_mg({-4, 5, -6}); // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-
+    // clears gif
+    pros::lcd::clear();
     while (true)
     {
+        chassis.tank(master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y), master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
         intakeControl();
     }
 }
